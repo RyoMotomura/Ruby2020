fab7aadbe31efac767ca2ef62a8f94729d2e382c
Ryo MOTOMURA <k20gjk06@st.kyusan-u.ac.jp>
2020-07-13 10:41:16 +0900

    HTTPハンドリングを追加

--- httpserver1.rb
+++ httpserver1.rb
@@ -1,22 +1,27 @@
 require 'socket'
 
 def server sock
+    body = ""
     while line=sock.gets
-        sleep 2
         line.chomp!
         break if line == ""
         p line
-        sock.puts "receive: #{line}"
+        body += "receive: #{line}<br>"
     end
+    sock.puts "HTTP/1.0 200 OK"
+    sock.puts ""
+    sock.puts "<!DOCTYPE html>"
+    sock.puts "<html><body>"
+    sock.puts "<h1>my server</h1>"
+    sock.puts body
+    sock.puts "</body></html>"
     sock.close
 end
 
-s = TCPServer.open 80
-
-while true
-    Thread.new s.accept do |sock|
-        server sock
+TCPServer.open 80 do |s|
+    while true
+        Thread.new s.accept do |sock|
+            server sock
+        end
     end
 end
-
-s.close
