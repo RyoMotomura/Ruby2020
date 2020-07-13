dd588dc44ecdbdf4b34b46ae3e026ee26744dcf1
Ryo MOTOMURA <k20gjk06@st.kyusan-u.ac.jp>
2020-07-13 10:04:21 +0900

    1st server

---
+++ httpserver1.rb
@@ -0,0 +1,15 @@
+require 'socket'
+
+s = TCPServer.open 80
+
+sock = s.accept
+while line=sock.gets
+    line.chomp!
+    break if line == ""
+    p line
+    sock.puts "receive: #{line}"
+end
+
+sock.close
+
+s.close
