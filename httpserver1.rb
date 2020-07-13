2c08657bceb85328d810f594a9e7cff515b05007
Ryo MOTOMURA <k20gjk06@st.kyusan-u.ac.jp>
2020-07-13 10:14:46 +0900

    クライアント処理をメソッドに分割

--- httpserver1.rb
+++ httpserver1.rb
@@ -1,16 +1,21 @@
 require 'socket'
 
-s = TCPServer.open 80
-
-while true
-    sock = s.accept
+def server sock
     while line=sock.gets
+        sleep 2
         line.chomp!
         break if line == ""
         p line
         sock.puts "receive: #{line}"
     end
     sock.close
+end
+
+s = TCPServer.open 80
+
+while true
+    sock = s.accept
+    server sock
 end
 
 s.close
